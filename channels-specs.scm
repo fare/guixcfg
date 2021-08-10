c9925fbf9d5cd4fd1f3c7325afdbc6566b5fbfeb
Francois-Rene Rideau <fare@tunes.org>
2021-08-09 22:18:35 -0400

    Add output of guix describe --format=channels

---
+++ channels-specs.scm
@@ -0,0 +1,15 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "e83fe81be4677aca1f42f0df9c8d2606400a4619")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'druix)
+        (url "https://github.com/drewc/druix.git")
+        (commit
+          "c5aa26ea91b3caea4aac03899d4d7731e0b6d2a9")))
