5df5f84167f2ab02eeee9b1b0c36374dd4667f19
Francois-Rene Rideau <fare@tunes.org>
2023-01-14 20:33:27 +0000

    deepness: document the failed attempts so far

--- deepness.scm
+++ deepness.scm
@@ -1,10 +1,18 @@
 ;; (non)guix configuration for Luna, a PINE64 PinePhone Pro
 ;;
-;; Built at this commit: guix pull --commit=v1.4.0 ;; 8e2f32cee982d42a79e53fc1e9aa7b8ff0514714
-;; NOT Built at this commit: guix pull --commit=eb5e650
-
-
-;; Trying to build it at the version-1.4.0 commit: 989a3916dc8967bcb7275f10452f89bc6c3389cc
+;; I tried in vain to build at this commit: guix pull --commit=v1.4.0
+;; aka 8e2f32cee982d42a79e53fc1e9aa7b8ff0514714 for which I was hoping
+;; to get more package substitutions. But that didn't help.
+;; Main blocker: tracker-3.3.3 won't pass a couple unit tests,
+;; so the entire gtk / gnome ecosystem fails, which breaks %desktop-services.
+;; guix build --without-tests=tracker works, but there is no such flag for guix system build.
+;;
+;; My current fallback is mobian (phosh) with guix as mere userland package manager.
+;; I intend to try sxmo instead of phosh at some point, and maybe use pmOS for that.
+;;
+;; TODO: try https://git.sr.ht/~oriansj/System_setup/tree/main/item/files/guix-config.scm
+;; as a template so I can have a working system without gtk? Or fork guix to disable tracker tests?
+;; or hack tracker to fix the tests?
 
 ;; References
 ;;
@@ -34,12 +42,12 @@
 
 (use-modules
   (gnu) (gnu bootloader u-boot) (gnu system nss)
-  (guix channels) (guix inferior) (guix modules)
-  ;;(nongnu packages fonts) ;; (nongnu packages linux) (nongnu system linux-initrd)
+  (guix channels) (guix inferior) (guix modules) (guix packages) (guix utils)
+  ;;(nongnu packages fonts) (nongnu packages linux) (nongnu system linux-initrd)
   (srfi srfi-1))
 
 (use-service-modules
-  authentication cups dbus desktop dict nix security-token sound ssh xorg);; virtualization docker
+  authentication cups dbus desktop dict nix security-token sddm sound ssh xorg);; virtualization docker
 
 (use-package-modules
   admin bootloaders certs cups fonts fontutils games ghostscript gnome linux lisp
@@ -54,6 +62,15 @@
         ;; font-alias font-awesome font-bitstream-vera font-gnu-freefont font-inconsolata font-mathjax
         ;; texlive-cm texlive-cm-super texlive-lm
         ))
+
+(define tracker/orig tracker)
+
+(define-public tracker/untested
+  (package (inherit tracker/orig)
+	   (arguments (substitute-keyword-arguments (package-arguments tracker/orig)
+						    ((#:tests? tests?) #f)))))
+(define make-tracker-untested
+  (package-input-rewriting `((,tracker ,tracker/untested)) #:deep? #t))
 
 (operating-system
   (host-name "deepness")
@@ -110,7 +127,8 @@
      (discard? #t)
      (dependencies mapped-devices))))
 
-  (kernel linux-libre-arm64-generic)
+  ;;(kernel linux-libre-arm64-generic)
+  (kernel linux-libre)
   (kernel-arguments
    '("iommu=soft"
      "nvme_core.default_ps_max_latency_us=0")) ;; needed to fix broken i660p m2 nvme disk on PinePhone
@@ -163,11 +181,11 @@
             ;; TODO: add to nonguix fortune-mod and other related packages that were removed from guix for offending the politically correct humorlessness of some maintainers? https://debbugs.gnu.org/cgi/bugreport.cgi?bug=54691 guix commits: f592decd4d823a973600d4cdd9f3f7d3ea610429 5e174805742b2bec9f5a06c9dd208c60b9af5c14 6b6b947b6133c40f86800dc0d36a59e16ac169fc
             #;TEXTDATA "dico" "daikichi" "fortunes-jkirchartz"
             #;TEXTUTILS "diffutils" "gawk" "sed" "m4" "perl" "patch" "recode" "wdiff"
-            #;LANGUAGES "make" "racket" "sbcl"
+            #;LANGUAGES "make" "sbcl" ;; "racket"
             #;VIDEO "vlc" ;; "mplayer"
             #;GRAPHICS "imagemagick" "feh" ;; "fbida" "gimp" "inkscape" "qiv"
             #;MARKUP "markdown" "python-docutils" ;; "texlive" ;; "guile-commonmark"
-            #;DOCUMENTS "evince" ;; "libreoffice" "xournal"
+	    #;DOCUMENTS "evince" ;; "libreoffice" "xournal"
             #;FONT-UTILS "xfontsel" "xlsfonts" ;; "gnome-font-viewer" "fontconfig" "fontmanager"
             #;XUTILS "xdpyinfo" "xmodmap" "xrandr" "xrdb" "xset" "xsetroot" "xwininfo"
             #;XPROGS "synergy" "stumpwm" "terminator" "xterm" "xscreensaver")) ;; "ratpoison"
@@ -175,7 +193,7 @@
 
   (services
     (cons*
-     (set-xorg-configuration
+      #;(set-xorg-configuration
         (xorg-configuration
           #;(server-arguments '()) ;; disable the default '("-nolisten" "tcp") ;--- don't do it until we have a good firewall!
           (keyboard-layout keyboard-layout)
@@ -188,7 +206,7 @@
            '("substituters = https://cache.nixos.org https://cache.nixos.org/ https://hydra.iohk.io https://iohk.cachix.org https://mukn.cachix.org\n"
              "trusted-substituters = https://cache.nixos.org https://cache.nixos.org/ https://hydra.iohk.io https://iohk.cachix.org https://mukn.cachix.org\n"
       "trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo= hydra.goguen-ala-cardano.dev-mantis.iohkdev.io-1:wh2Nepc/RGAY2UMvY5ugsT8JOz84BKLIpFbn7beZ/mo= mukn.cachix.org-1:ujoZLZMpGNQMeZbLBxmOcO7aj+7E5XSnZxwFpuhhsqs=\n"))))
-      (dicod-service #:config
+      #;(dicod-service #:config
        (dicod-configuration
         (databases (list ;; TODO: package and add other databases?
                     %dicod-database:gcide))))
@@ -201,7 +219,7 @@
             `(("fare" ,(local-file "/home/fare/.ssh/id_rsa.pub"))
               ("root" ,(local-file "/home/fare/.ssh/id_rsa.pub"))))))
       #;(service fprintd-service-type)
-      (service pcscd-service-type)
+      #;(service pcscd-service-type)
       #;(pam-limits-service
         (list
           (pam-limits-entry "@audio" 'both 'rtprio 99)
@@ -210,19 +228,20 @@
       #;(service virtlog-service-type (virtlog-configuration (max-clients 1000)))
       #;(service singularity-service-type)
       #;(service docker-service-type)
-      (service cups-service-type
+      #;(service cups-service-type
         (cups-configuration
           (web-interface? #t)
           (extensions
            (list brlaser cups-filters epson-inkjet-printer-escpr foomatic-filters
                  hplip-minimal splix)))) ;; gutenprint
-      (modify-services %desktop-services ;; %base-services
-        (delete gdm-service-type) (delete gdm-file-system-service)
+      (modify-services %base-services ;; %desktop-services %base-services
+        (delete gdm-service-type) (delete gdm-file-system-service) (delete sddm-service-type)
         (console-font-service-type _config =>
           (map (lambda (i)
                  (cons (format #f "tty~d" i)
                        (file-append font-terminus "/share/consolefonts/ter-i24b.psf.gz")))
                (iota 6 1)))
+
         (guix-service-type config =>
           (guix-configuration
             (inherit config)
